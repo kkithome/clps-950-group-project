e873576fba8fcce5b2a995342057c0452b631bd6
kkithome <117781574+kkithome@users.noreply.github.com>
2025-03-18 17:44:29 -0400

    added a shuffle button for the words and modified the code accordingly

--- our-code/connections.m
+++ our-code/connections.m
@@ -13,6 +13,9 @@ function connections()
     % Reshape into a 4x4 matrix for display
     shuffledWords = reshape(shuffledWords, 4, 4)';
 
+    %Shuffle Button
+
+    
 
     % figure fot the welcome page
     fig1 = uifigure('Name', 'Welcome Page', 'Position', [100, 100, 600, 600]);
@@ -32,19 +35,66 @@ function connections()
     function startGame(~, ~)
         fig1.Visible = 'off';
         fig2.Visible = 'on';
+        displayGrid()
     end
 
-    % creates the start button and when clicked switches from the welcome page to the grid
-    uibutton(fig1, 'push', ...
-        'Text', 'Click to Play!', ...
-        'Position', [250, 200, 100, 30], ...
-        'ButtonPushedFcn', @startGame);
+    function displayGrid()
+        delete(grid.Children);
+        for row = 1:4
+            for col = 1:4
+                wordButton = uibutton(grid, 'push', ...
+                'Text', shuffledWords{row, col}, ...
+                'ButtonPushedFcn', @(btn, ~) wordButtonPushed(btn));
+                wordButton.Layout.Row = row;
+                wordButton.Layout.Column = col;
+            end
+        end
+    end
+
+
+    function shuffleWords()
+        shuffledWords = allWords(randperm(length(allWords)));
+        shuffledWords = reshape(shuffledWords, 4, 4);
+        displayGrid();
+    end
 
     % Create a grid layout for the words
     grid = uigridlayout(fig2, [4, 4]);
     grid.RowHeight = {'5x', '5x', '5x', '5x'};
     grid.ColumnWidth = {'5x', '5x', '5x', '5x'};
 
+
+    % ---- Button Creation --------
+
+    % creates the start button and when clicked switches from the welcome page to the grid
+    uibutton(fig1, 'push', ...
+        'Text', 'Click to Play!', ...
+        'Position', [100, 50, 100, 30], ...
+        'ButtonPushedFcn', @startGame);
+
+   % Add a "Submit Button
+    uibutton (fig2, 'push', ...
+        'Text', 'Submit', ...
+        'Position', [190, 30, 100, 30], ...
+        'ButtonPushedFcn', @(selectedWords, event) submitSelection(selectedWords));
+     
+
+    % Add a "Close" button
+    uibutton(fig2, 'push', ...
+        'Text', 'Close', ...
+        'Position', [20, 20, 100, 30], ...
+        'ButtonPushedFcn', @(btn, event) close(fig2));
+
+
+    % Add a "Shuffle Words" Button
+    uibutton(fig2, 'push', ...
+        'Text', 'Shuffle Words',...
+        'Position', [100, 10, 100, 30], ...
+        'ButtonPushedFcn', @(btn, event) shuffleWords());
+        
+
+    
+    
     function wordButtonPushed(btn)
         if ismember(btn.Text, selectedWords) % checks to see if the word has been selected already
             selectedWords(strcmp(selectedWords, btn.Text)) = [];
@@ -55,17 +105,7 @@ function connections()
         end
     end
 
-    % Add buttons for each word in the grid
-    for row = 1:4
-        for col = 1:4
-            % Create a button for each word
-            wordButton = uibutton(grid, 'push', ...
-                'Text', shuffledWords{row, col}, ...
-                'ButtonPushedFcn', @(btn, ~) wordButtonPushed(btn));
-            wordButton.Layout.Row = row;
-            wordButton.Layout.Column = col;
-        end
-    end
+ 
 
     % When a word is selected, the game should:
         % if less than four words are selected:
@@ -75,15 +115,10 @@ function connections()
             % tells the user that four words
 
     selectedWords = {}; 
-        
 
-    % Add a "Close" button
-    uibutton(fig2, 'push', ...
-        'Text', 'Close', ...
-        'Position', [250, 0, 100, 30], ...
-        'ButtonPushedFcn', @(btn, event) close(fig2));
-
-
+    function submitSelection(selectedWords)
+    end
+  
 
 
 
